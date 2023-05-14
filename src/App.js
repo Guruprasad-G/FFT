cee17ecdf6a6295297bae28f27797fbaff540ed6
Guruprasad G <guruprasad2018g@gmail.com>
2023-05-14 17:26:45 +0530

    Reinitalizing code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import NavBar from '../src/components/NavBar/NavBar'
-import MainPage from './components/MainPage/MainPage';
-import { Container } from '@mui/system';
 
 function App() {
   return (
-    <>
-      <NavBar></NavBar>
-      <MainPage  style={{margintop:"500px"}}></MainPage>
-    </>
+    <h1>Hi</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import NavBar from '../src/components/NavBar/NavBar'
-import MainPage from '../src/components/MainPage/MainPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar></NavBar>
-    <MainPage></MainPage>
+    <App />
   </React.StrictMode>
 );
 
