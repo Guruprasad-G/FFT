488dfc6736c76c8fca09b8e9276eb5e652ac2427
Guruprasad G <guruprasad2018g@gmail.com>
2023-11-26 14:51:32 +0530

    Adding only butterfly.js dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 // import MainPage from './components/MainPage/MainPage';
-import { Graph } from "react-d3-graph";
+// import { Graph } from "react-d3-graph";
 import { Butterfly } from './Butterfly'
-import { Point8DIFFFTorDITIFFT, Point8DITFFTorDIFIFFT } from './EightPoint'
-import { Point4DIFFFTorDITIFFT, Point4DITFFTorDIFIFFT } from './FourPoint'
-import { complex, conj, multiply, sqrt } from 'mathjs'
+// import { Point8DIFFFTorDITIFFT, Point8DITFFTorDIFIFFT } from './EightPoint'
+// import { Point4DIFFFTorDITIFFT, Point4DITFFTorDIFIFFT } from './FourPoint'
+// import { complex, conj, multiply, sqrt } from 'mathjs'
 // import { Container, Row, Col, Spinner, Card, Button, Form  } from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive'
 
