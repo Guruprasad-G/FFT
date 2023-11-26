66de2f9d77fa61c9a65836034b935627458d78dd
Guruprasad G <guruprasad2018g@gmail.com>
2023-11-26 15:09:05 +0530

    Brought back MUI version of Main.js

--- src/components/MainPage/MainPagewithBootstrap.js
+++ src/components/MainPage/MainPagewithBootstrap.js
@@ -1,23 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Butterfly } from './Butterfly'
 import { Point8DIFFFTorDITIFFT, Point8DITFFTorDIFIFFT } from './EightPoint'
 import { Point4DIFFFTorDITIFFT, Point4DITFFTorDIFIFFT } from './FourPoint'
 import { complex, conj, multiply, sqrt } from 'mathjs'
-import { Button, Card, CircularProgress, Container, CardMedia, Grid, useMediaQuery } from '@material-ui/core'
-
-// const Item = 
-// (Paper)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(2),
-//     textAlign: 'center',
-// }));
+import { Container, Row, Col, Spinner, Card, Button, Form  } from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive'
 
 const Userinputform1 = ({ onChange }) => {
     return (
         <Container style={{color:"inherit"}}>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                <Grid item xs={2} sm={4} md={4}>
+            <Row xs={2} md={3}>
+                <Col xs={4} sm={8} md={12}>
                     {/* <Item> */}
                         <label for="4points"> 4 point Input</label>
                         <input type="radio" value="4" placeholder="DFT" name="points" onChange={onChange} id="4points"></input>
@@ -25,23 +18,23 @@ const Userinputform1 = ({ onChange }) => {
                         <label for="8points"> 8 point Input</label>
                         <input type="radio" value="8" placeholder="IDFT" name="points" onChange={onChange} id="8points"></input>
                         {/* </Item> */}
-                </Grid>
-                <Grid item xs={2} sm={4} md={4}>
+                </Col>
+                <Col xs={4} sm={8} md={12}>
                     {/* <Item> */}
                         <label for="FFTtransform">FFT</label>
                         <input type="radio" value="true" placeholder="DFT" name="transform" onChange={onChange} id="FFTtransform"></input>
                         <label for="IFFTtransform">  IFFT</label>
                         <input type="radio" value="false" placeholder="IDFT" name="transform" onChange={onChange} id="IFFTtransform"></input>
                         {/* </Item> */}
-                </Grid>
-                <Grid item xs={2} sm={4} md={4}>
+                </Col>
+                <Col xs={4} sm={8} md={12}>
                     {/* <Item> */}
                         <label>Using DIT or DIF</label>
                         <input type="radio" value="true" placeholder="DIT" name="type" onChange={onChange}></input>
                         <input type="radio" value="false" placeholder="DIF" name="type" onChange={onChange}></input>
                         {/* </Item> */}
-                </Grid>
-            </Grid>
+                </Col>
+            </Row>
         </Container>
     )
 }
@@ -50,10 +43,24 @@ const Advertisement = ({height, width}) => {
     const [loading, setLoading] = useState(true);
     const [hasError, setHasError] = useState(false);
     return (
-        <Container style={{height:height, width:width, padding:"0"}} >
-        { hasError ? <p>The iframe could not be loaded.</p> : loading ? <div style={{ alignItems: "center", justifyContent: "center" }}>
-            <CircularProgress></CircularProgress></div> : <CardMedia style={{ height: "100%", width: "100%", padding: "0" }} component="iframe" src="https://www.youtube.com/embed/ziQ9GURNrUg/" onError={(e) => {setHasError(true)}} onLoad={() => setLoading(false)}></CardMedia> }
-        </Container>
+<Container style={{ height: height, width: width, padding: "0" }}>
+  {hasError ? (
+    <p>The iframe could not be loaded.</p>
+  ) : loading ? (
+    <div style={{ alignItems: "center", justifyContent: "center" }}>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  ) : (
+    <Card style={{ height: "100%", width: "100%", padding: "0" }}>
+      <Card.Img>
+      <iframe title="Embedded Video" src="https://www.youtube.com/embed/ziQ9GURNrUg/" style={{ height: "100%", width: "100%" }} onError={(e) => { setHasError(true); }} onLoad={() => setLoading(false)} ></iframe>
+      </Card.Img>
+    </Card>
+  )}
+</Container>
+
     )
 }
 
@@ -63,14 +70,20 @@ const Userinputform2 = ({points, onChange, field1}) => {
         {
             points ? points.map((numobject) => {
                 return (
-                    <Grid container style={{ alignItems: "center", justifyContent: "center" }}>
-                        <Grid item xs="auto" md="auto">
-                            <br></br>
-                            <label>x({numobject.index}) = </label>
-                            <input type="number" name="realinput" defaultValue={0} onChange={onChange} id={numobject.index}></input>
-                            {field1.transform ? null : <>+<input type="number" name="imginput" defaultValue={0} onChange={onChange} id={numobject.index}></input><button disabled style={{ color: "black" }}>i</button></>}
-                        </Grid>
-                    </Grid>
+                    <Container style={{ alignItems: "center", justifyContent: "center" }}>
+  <Row>
+    <Col xs="auto" md="auto">
+      <br />
+      <Form.Label>{`x(${numobject.index}) = `}</Form.Label>
+      <Form.Control type="number" name="realinput" defaultValue={0} onChange={onChange} id={numobject.index}/>
+      {!field1.transform ? (
+        <> + <Form.Control type="number" name="imginput" defaultValue={0} onChange={onChange} id={numobject.index}/>
+          <button disabled style={{ color: "black" }}>i</button>
+        </>
+      ) : null}
+    </Col>
+  </Row>
+</Container>
                 )
             }) : null
         }
@@ -97,7 +110,7 @@ const MainPage = () => {
     const [points, updatepoints] = useState([])
     const [errors, updateerrors] = useState(null)
     const [output, updateoutput] = useState(null)
-    const screenWidth = useMediaQuery('(min-width:600px)');
+    const screenWidth = useMediaQuery({ query: '(min-width: 600px)' });
     const Errors = {
         PointsError: "Select number of points",
         TransformError: "Select a transform",
